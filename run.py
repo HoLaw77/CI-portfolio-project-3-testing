c668afcdbc5ecd507f4aa032e6b3924976162dcb
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-04-14 18:56:38 +0000

    define survey_input function for user to fill in survey for providing data

--- run.py
+++ run.py
@@ -13,4 +13,26 @@ SHEET = GSPREAD_CLIENT.open('lunch_survey')
 
 sales = SHEET.worksheet('sales')
 data = sales.get_all_values()
-print(data) 
+
+def survey_input():
+    """
+    Create input for user to input survey data
+    """
+    while True:
+        print('It is an anonymous survey on lunch choice. we do not need to know your name.')
+        print('But we need your age, gender, Food choice and are you willing to buy again')
+        print('Food choice: Fish and Chip/Salad/Sandwich/Noodle')
+        print('buy again: yes/no')
+
+        age_input_str = input("Enter your age here:\n")
+        gender_input_str = input("Enter your gender here:\n")
+        Food_Choice_input_str = input("Enter your food choice here:\n")
+        buy_again_input_str = input("Enter your if you will buy again here:\n")
+
+        print(f' you are {age_input_str} years old')
+        print(f' your gener is {gender_input_str}')
+        print(f' your lunch choice is {Food_Choice_input_str}') 
+        print(f' you answer {buy_again_input_str} for buying again')
+    
+survey_data = survey_input()
+print(survey_data)
