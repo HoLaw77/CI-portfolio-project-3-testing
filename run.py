a894cce699fea4fea8f2161b8cc793e683b0e7cc
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-04-14 19:01:02 +0000

    add \n to each line of print

--- run.py
+++ run.py
@@ -19,20 +19,20 @@ def survey_input():
     Create input for user to input survey data
     """
     while True:
-        print('It is an anonymous survey on lunch choice. we do not need to know your name.')
-        print('But we need your age, gender, Food choice and are you willing to buy again')
-        print('Food choice: Fish and Chip/Salad/Sandwich/Noodle')
-        print('buy again: yes/no')
+        print('It is an anonymous survey on lunch choice. we do not need to know your name.\n')
+        print('But we need your age, gender, Food choice and are you willing to buy again\n')
+        print('Food choice: Fish and Chip/Salad/Sandwich/Noodle\n')
+        print('buy again: yes/no\n')
 
         age_input_str = input("Enter your age here:\n")
         gender_input_str = input("Enter your gender here:\n")
         Food_Choice_input_str = input("Enter your food choice here:\n")
         buy_again_input_str = input("Enter your if you will buy again here:\n")
 
-        print(f' you are {age_input_str} years old')
-        print(f' your gener is {gender_input_str}')
-        print(f' your lunch choice is {Food_Choice_input_str}') 
-        print(f' you answer {buy_again_input_str} for buying again')
+        print(f' you are {age_input_str} years old\n')
+        print(f' your gener is {gender_input_str}\n')
+        print(f' your lunch choice is {Food_Choice_input_str}\n') 
+        print(f' you answer {buy_again_input_str} for buying again\n')
     
 survey_data = survey_input()
 print(survey_data)
