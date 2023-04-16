2427602743f25d71e473403cb68256f6f917dc16
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-04-16 16:20:03 +0000

    fix problem for update google sheet

--- run.py
+++ run.py
@@ -33,52 +33,53 @@ def survey_input():
         print(f' you answer {buy_again_input_str} for buying again\n')
 
         lunch_choice = {}
-        lunch_choice["age"] = age_input_str
-        lunch_choice["gender"] = gender_input_str
-        lunch_choice["food_choice"] = Food_Choice_input_str
-        lunch_choice["buy_again_choice"] = buy_again_input_str
+        lunch_choice["Age"] = age_input_str
+        lunch_choice["Gender"] = gender_input_str
+        lunch_choice["Food_Choice"] = Food_Choice_input_str
+        lunch_choice["Buy_Again_Choice"] = buy_again_input_str
         #for n in (age_input_str, gender_input_str, Food_Choice_input_str, buy_again_input_str):
         #    lunch_choice.append(n)
         print(lunch_choice)
         if validate_survey(lunch_choice):
             print("survey input valid.")
             break
+    return lunch_choice
 
 def validate_survey(values):
     """
     Test if each value type matches. String for gender, food_choice, buy_again_choice, and number for age in the try statement 
     """
     try: 
-        if values["age"].isnumeric() != True:
+        if values["Age"].isnumeric() != True:
             raise ValueError(
-                f'Exact number is required for age, you have entered {values["age"]}'
+                f'Exact number is required for age, you have entered {values["Age"]}'
                 )
     except ValueError as e:
         print(f'Invalid data: {e}, please try again\n')
         return False      
 
     try:
-        if values["gender"] not in ("Male", "Female"):
+        if values["Gender"] not in ("Male", "Female"):
             raise ValueError(
-                f'You can only be either Male or Female, you have entered {values["gender"]}'
+                f'You can only be either Male or Female, you have entered {values["Gender"]}'
             )      
     except ValueError as e:
         print(f'Invalid data: {e}, please try again\n')
         return False
     
     try:
-        if values["food_choice"] not in ("Fish and Chip", "Salad", "Sandwich", "Noodle"):
+        if values["Food_Choice"] not in ("Fish and Chip", "Salad", "Sandwich", "Noodle"):
             raise ValueError(
-                f'We are researching on the most common lunch type you can get on Tesco, you have chosen {values["food_choice"]}'
+                f'We are researching on the most common lunch type you can get on Tesco, you have chosen {values["Food_Choice"]}'
             )     
     except ValueError as e:
         print(f'Invalid data: {e}, please try again\n')
         return False
     
     try:
-        if values["buy_again_choice"] not in ("Yes", "No"):
+        if values["Buy_Again_Choice"] not in ("Yes", "No"):
             raise ValueError(
-                f'You can either say yes or no for buy again, you have chosen {values["buy_again_choice"]}'
+                f'You can either say yes or no for buy again, you have chosen {values["Buy_Again_Choice"]}'
             )     
     except ValueError as e:
         print(f'Invalid data: {e}, please try again\n')
@@ -90,9 +91,10 @@ def update_worksheet(data):
     Update google spreadsheet for survey input
     """
     survey_worksheet = SHEET.worksheet("sales")
-    survey_worksheet.append_row(data)
+    survey_worksheet.worksheet.append_column(data)
     print("updating worksheet")
     print("Worksheet updated successfully.\n")
+
 
 data = survey_input()
 lunch_survey_data = [i for i in "lunch_choice"]
