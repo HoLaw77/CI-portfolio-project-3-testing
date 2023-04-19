ea7449297f56aa98fa0b6e1dd56c36f35e5e5e1e
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-04-19 18:56:30 +0000

    build validate function to validate input for survey

--- run.py
+++ run.py
@@ -31,7 +31,6 @@ def survey_input():
         print(f' your gener is {gender_input_str}\n')
         print(f' your lunch choice is {Food_Choice_input_str}\n') 
         print(f' you answer {buy_again_input_str} for buying again\n')
-
         lunch_choice = []
         lunch_choice.append(age_input_str)
         lunch_choice.append(gender_input_str)
@@ -50,42 +49,17 @@ def validate_survey(values):
     """
     Test if each value type matches. String for gender, food_choice, buy_again_choice, and number for age in the try statement 
     """
-    try: 
-        if values["Age"].isnumeric() != True:
+    try:
+        if {age_input_str}.isdigit() != True:
             raise ValueError(
-                f'Exact number is required for age, you have entered {values["Age"]}'
-                )
-    except ValueError as e:
-        print(f'Invalid data: {e}, please try again\n')
-        return False      
+                f'Please provide exact number for age. you provided {age_input_str}'
+            )
+        except ValueError as e:
+            print(f"Invalid data: {e}, Please try again.")
+        
+        if {gender_input_str} != 
 
-    try:
-        if values["Gender"] not in ("Male", "Female"):
-            raise ValueError(
-                f'You can only be either Male or Female, you have entered {values["Gender"]}'
-            )      
-    except ValueError as e:
-        print(f'Invalid data: {e}, please try again\n')
-        return False
-    
-    try:
-        if values["Food_Choice"] not in ("Fish and Chip", "Salad", "Sandwich", "Noodle"):
-            raise ValueError(
-                f'We are researching on the most common lunch type you can get on Tesco, you have chosen {values["Food_Choice"]}'
-            )     
-    except ValueError as e:
-        print(f'Invalid data: {e}, please try again\n')
-        return False
-    
-    try:
-        if values["Buy_Again_Choice"] not in ("Yes", "No"):
-            raise ValueError(
-                f'You can either say yes or no for buy again, you have chosen {values["Buy_Again_Choice"]}'
-            )     
-    except ValueError as e:
-        print(f'Invalid data: {e}, please try again\n')
-        return False
-    return True
+
 
 def update_worksheet(data):
     """
