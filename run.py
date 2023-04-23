c9a2b013b98ec526bca5174c6e0b78fb1b618eb0
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-04-23 19:38:27 +0000

    delete pprint statement and change the setting for number_Salad to get the data in the sheet

--- run.py
+++ run.py
@@ -83,24 +83,22 @@ def Get_popular_product(data):
     fish_and_chip_sales = SHEET.worksheet("fish_and_chip_sales").get_all_values()
     noodle_sales = SHEET.worksheet("noodle_sales").get_all_values()
     sandwich_sales = SHEET.worksheet("sandwich_sales").get_all_values()
-    pprint(sales)
+    
 
 def update_Noodles_sales(data):
     """
     import sales data limited to noodles to worksheet
     """
     if data[2] == "Noodles":
-        if data [3] == "Yes":
             noodle_sheet = SHEET.worksheet("noodle_sales")
             noodle_sheet.append_row(data)
             print("noodle_sales sheet successfully updated")
-    print(noodle_sheet)
+    
 def update_salad_sales(data):
     """
     import sales data limited to salad to worksheet
     """
     if data[2] == "salad":
-        if data [3] == "Yes":
             salad_sheet = SHEET.worksheet("salad_sales")
             salad_sheet.append_row(data)
             print("salad_sales sheet successfully updated")
@@ -110,7 +108,6 @@ def update_sandwich_sales(data):
     import sales data limited to sandwich to worksheet
     """
     if data[2] == "sandwich":
-        if data [3] == "Yes":
             sandwich_sheet = SHEET.worksheet("sandwich_sales")
             sandwich_sheet.append_row(data)
             print("sandwich_sales sheet successfully updated")
@@ -120,11 +117,17 @@ def update_Fish_and_Chip_sales(data):
     import sales data limited to Fish and Chips to worksheet
     """
     if data[2] == "Fish and Chip":
-        if data [3] == "Yes":
             fish_and_chip_sheet = SHEET.worksheet("fish_and_chip_sales")
             fish_and_chip_sheet.append_row(data)
             print("fish_and_chip_sales sheet successfully updated")
     
+def survey_result(data):
+    """
+    Count the number of each food choice and print the result in console
+    """
+    number_salad = SHEET.worksheet("salad_sales")   
+    print(f'The number of people who wants to buy sandwich again is {number_salad}')
+
 def main():
     """Run all programme function"""
     data = survey_input()
@@ -135,5 +138,6 @@ def main():
     update_Noodles_sales(data)
     update_sandwich_sales(data)
     update_Fish_and_Chip_sales(data)
+    survey_result(data)
 print("Welcome to Lunch Survery Data Automation\n")
 main()
