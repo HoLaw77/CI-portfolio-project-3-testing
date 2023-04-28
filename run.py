97d642dfda7b0ca673f419520e2479e2fc0f975a
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-04-28 20:17:11 +0000

    clear problems too long line

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-#external code suggested from tutor for importing mean
 from statistics import mean
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -20,17 +19,17 @@ def survey_input():
     """
     while True:
         print('It is an anonymous survey on lunch choice.\n')
-        print('But we need your age, gender, Food choice and are you willing to buy again\n')
+        print('We need your age, gender, food choice and will you buy later\n')
         print('Food choice: Fish and Chip/Salad/Sandwich/Noodle\n')
         print('buy again: yes/no\n')
 
         age_input_str = input("Enter your age here:\n")
-        while age_input_str.isnumeric() != True:
+        while age_input_str.isnumeric() is not True:
             age_input_str = input("Error, please provide a number for age:")
         gender_input_str = input("Enter your gender here: male or female\n")
         while gender_input_str not in ("male", "female"):
             gender_input_str = input("Error, please enter your gender again:")
-        food_choice_input_str = input("Enter your food choice here: salad or noodle or sandwich or fish and chip\n")
+        food_choice_input_str = input("Enter your food choice: salad, noodle, sandwich, fish and chip\n")
         while food_choice_input_str not in ("salad", "noodle", "sandwich", "fish and chip"):
             food_choice_input_str = input("Error, please enter the provided food choice")
         buy_again_input_str = input("Enter your if you will buy again here: yes or no\n")
@@ -47,8 +46,6 @@ def survey_input():
         lunch_choice.append(food_choice_input_str)
         lunch_choice.append(buy_again_input_str)
         print(lunch_choice)
-        # for n in (age_input_str, gender_input_str, Food_Choice_input_str, buy_again_input_str):
-        #    lunch_choice.append(n)
         print(lunch_choice)
         break
 
@@ -64,6 +61,7 @@ def update_worksheet(data):
     print("updating worksheet")
     print("Worksheet updated successfully.\n")
 
+
 def update_Noodles_sales(data):
     """
     import sales data limited to noodles to worksheet
@@ -73,6 +71,7 @@ def update_Noodles_sales(data):
         noodle_sheet.append_row(data)
         print("noodle_sales sheet successfully updated")
     
+
 def update_salad_sales(data):
     """
     import sales data limited to salad to worksheet
@@ -81,6 +80,7 @@ def update_salad_sales(data):
         salad_sheet = SHEET.worksheet("salad_sales")
         salad_sheet.append_row(data)
         print("salad_sales sheet successfully updated")
+
 
 def update_sandwich_sales(data):
     """
@@ -91,6 +91,7 @@ def update_sandwich_sales(data):
         sandwich_sheet.append_row(data)
         print("sandwich_sales sheet successfully updated")
 
+
 def update_Fish_and_Chip_sales(data):
     """
     import sales data limited to Fish and Chips to worksheet
@@ -99,7 +100,8 @@ def update_Fish_and_Chip_sales(data):
         fish_and_chip_sheet = SHEET.worksheet("fish_and_chip_sales")
         fish_and_chip_sheet.append_row(data)
         print("fish_and_chip_sales sheet successfully updated")
-    
+
+
 def survey_result(data):
     """
     Count the number of each food choice and print the result in console
@@ -113,7 +115,7 @@ def survey_result(data):
     flat_ages = [int(age) for sublist in ages for age in sublist]
     # Now calculate the mean
     average_age = int(mean(flat_ages))
-    print(f'The number of people who wants to buy salad again is {number_salad}, male:{male_salad}, female:{female_salad}, average-age: {average_age}')
+    print(f'Buy salad again: {number_salad}, male:{male_salad}, female:{female_salad}, average-age: {average_age}')
     
     fish_and_chip_result = SHEET.worksheet("fish_and_chip_sales").get_all_values()
     number_fish_and_chip = sum(num.count("Yes") for num in fish_and_chip_result) 
@@ -124,7 +126,7 @@ def survey_result(data):
     flat_ages = [int(age) for sublist in ages for age in sublist]
     # Now calculate the mean
     average_age = int(mean(flat_ages)) 
-    print(f'The number of people who wants to buy fish and chip again is {number_fish_and_chip}, male:{male_fish_and_chip}, female:{female_fish_and_chip}, average_age: {average_age}')
+    print(f'Buy fish and chip again:{number_fish_and_chip}, male:{male_fish_and_chip}, female:{female_fish_and_chip}, average_age: {average_age}')
 
     sandwich_result = SHEET.worksheet("sandwich_sales").get_all_values()
     number_sandwich = sum(num.count("Yes") for num in sandwich_result)
@@ -135,7 +137,7 @@ def survey_result(data):
     flat_ages = [int(age) for sublist in ages for age in sublist]
     # Now calculate the mean
     average_age = int(mean(flat_ages)) 
-    print(f'The number of people who wants to buy sandwich again is {number_sandwich}, male:{male_sandwich}, female:{female_sandwich}, age: {average_age}')
+    print(f'Buy sandwich again:{number_sandwich}, male:{male_sandwich}, female:{female_sandwich}, age: {average_age}')
 
     noodle_result = SHEET.worksheet("noodle_sales").get_all_values()
     number_noodle = sum(num.count("Yes") for num in noodle_result)
@@ -146,7 +148,7 @@ def survey_result(data):
     flat_ages = [int(age) for sublist in ages for age in sublist]
     # Now calculate the mean
     average_age = int(mean(flat_ages)) 
-    print(f'The number of people who wants to buy noodle again is {number_noodle}, male:{male_noodle}, female:{female_noodle}, average-age:{average_age}')
+    print(f'Buy noodle again{number_noodle} male:{male_noodle}, female:{female_noodle}, average-age:{average_age}')
 
 
 def main():
@@ -166,5 +168,7 @@ def main():
         if ask == "no":
             survey_end = False
             print("Thanks for joining, have a good day.")
+
+
 print("Welcome to Lunch Survery Data Automation\n")
 main()
